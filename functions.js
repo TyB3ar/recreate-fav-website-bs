3e8397d2cd6a61bbf68298214d22352d41e79566
Tyler Wronski <wronskityler@gmail.com>
2025-03-18 10:40:50 -0700

    First Commit

---
+++ functions.js
@@ -0,0 +1,7 @@
+// Functions to implement later on: 
+
+// Modal Function for Pop-up Modal on 'Play Now' Button
+
+// Carousel Function to rotate cards in carousel 
+
+// Form Validation for UserName 
