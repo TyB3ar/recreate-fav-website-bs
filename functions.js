cc8e1eeb171cdf237cad70af31ad14e1dd4d0c40
Tyler Wronski <wronskityler@gmail.com>
2025-03-19 14:26:48 -0700

    Added More JS functions, changed Login Form to Register Form

--- functions.js
+++ functions.js
@@ -1,5 +1,11 @@
 // Form Validation for UserName and Password
 const form = document.getElementById("login-form");
+
+// Array for User Info (Username and Password)
+const regUser = [
+    {userId: 'username', password : 'password'}
+]; 
+
 form.addEventListener("submit", function (e) {
     e.preventDefault();
     const user = document.getElementById("username").value;
@@ -15,6 +21,9 @@ form.addEventListener("submit", function (e) {
         document.getElementById("username-error").textContent = 
             "Username must be 3-15 characters (Letters, Numbers, Underscores).";
         isValid = false;
+    } else{
+        isValid = true; 
+        regUser.push(regUser.userId, user) // If username is valid, add username to regUser array 
     }
 
     const passRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%?&])[A-Za-z\d@$!%?&]{8,}$/;
@@ -22,7 +31,15 @@ form.addEventListener("submit", function (e) {
         document.getElementById("password-error").textContent = 
             "Password must have 8+ characters (1 uppercase, 1 number, 1 special character)";
         isValid = false;
+    } else {
+        isValid = true;
+        regUser.push(regUser.password, pass) // if password is valid, add password to regUser array 
     }
+
 });
 
-// Fetch User Data for Login (Username and Password) 
+// Check Username and Password
+
+const users = regUser.find(
+    u => u.userId === form.user.value && 
+         u.password === form.pass.value); 
